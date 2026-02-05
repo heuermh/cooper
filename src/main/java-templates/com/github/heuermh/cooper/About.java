eea5c4077992c509687e14b7f1a4be55a90d20c2
Michael L Heuer <heuermh@acm.org>
2026-02-05 10:56:55 -0600

    update copyright years to include 2026

--- src/main/java-templates/com/github/heuermh/cooper/About.java
+++ src/main/java-templates/com/github/heuermh/cooper/About.java
@@ -25,7 +25,7 @@ public final class About implements IVersionProvider {
     private static final String BUILD_TIMESTAMP = "${build-helper-maven-plugin.build.timestamp}";
     private static final String COMMIT = "${git.commit.id}";
     private static final String DOI = "10.5281/zenodo.14976257";
-    private static final String COPYRIGHT = "Copyright (c) 2024-2025 held jointly by the individual authors.";
+    private static final String COPYRIGHT = "Copyright (c) 2024-2026 held jointly by the individual authors.";
     private static final String LICENSE = "Licensed Apache License 2.0.";
     private static final String VERSION = "${project.version}";
 
